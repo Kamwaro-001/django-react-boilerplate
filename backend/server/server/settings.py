8aad0a00cc852b458db055338cc0ace172a47fee
Kamwaro-001 <jobkamwaro9@gmail.com>
2022-10-08 21:51:51 +0300

    adding docker compose pt1

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 # CORS ORIGINS
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
 ]
 
 #configure DRF
@@ -154,7 +154,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+STATIC_URL = 'django_static/'
+STATIC_ROOT = BASE_DIR / 'django_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
